d7247e4eaf35451d2951ba7aaa518d74eeb92d75
Jared Girvan <jaredgirvan@Jareds-MBP.fios-router.home>
2022-06-11 14:55:47 -0400

    Final Portfolio with javascript btn

--- pages/script.js
+++ pages/script.js
@@ -1,9 +1,32 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
+
+
+
+
+let myButton = document.getElementById('myBtn');
+
+window.onscroll = function() {
+  scrollFunction()
+};
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
   }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
